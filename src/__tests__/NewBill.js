35ca8bb976e35d98a8e6023d00093dd2fc515048
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-02-20 15:28:14 +0100

    test handleChangeFile

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,7 +5,7 @@
 import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-import { ROUTES, ROUTES_PATH } from "../constants/routes"
+import { ROUTES } from "../constants/routes"
 import { localStorageMock } from "../__mocks__/localStorage.js"
 
 describe("Given I am connected as an employee", () => {
@@ -16,6 +16,28 @@ describe("Given I am connected as an employee", () => {
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({pathname})
       }
+
+      const newBill = new NewBill({document, onNavigate, store})
+      const handleChangeFile = jest.fn(newBill.handleChangeFile)
+      const fileInput = screen.getByTestId('file')
+      const inputData = {
+        name: 'jane-roe.jpg',
+        _lastModified: 1580400631732,
+        get lastModified() {
+          return this._lastModified
+        },
+        set lastModified(value) {
+          this._lastModified = value
+        },
+        size: 703786,
+        type: 'image/jpeg'
+      }
+      const file = screen.getByTestId("file")
+      fileInput.addEventListener("change", handleChangeFile)
+      fireEvent.change(file, { target: { files: [inputData] } })
+
+      expect(handleChangeFile).toHaveBeenCalled()
+      expect(store.storage.put).toHaveBeenCalled()
     })
   })
    // POST new bill
